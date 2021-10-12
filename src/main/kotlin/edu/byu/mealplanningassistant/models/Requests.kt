bcc4a3a15a943182871ba97413f67b46bcb11472
Chris West <64553509+Chris4942@users.noreply.github.com>
2021-10-12 17:08:25 -0600

    modified request objects (#8)
    * modified request objects
    
    * updated response and requests object with the other architects

--- src/main/kotlin/edu/byu/mealplanningassistant/models/Requests.kt
+++ src/main/kotlin/edu/byu/mealplanningassistant/models/Requests.kt
@@ -1,39 +1,42 @@
 package edu.byu.mealplanningassistant.models
 
+abstract class Response(
+    open val success: Boolean,
+    open val message: String = "Unknown",
+)
+
 class CreateRecipeRequest (
     val name: String,
-    val instructions: String,
-    val attributes: List<String>,
+    val owner: String,
+    val ingredients: Map<String, String>,
+    val instructions: List<String>,
+    val tags: List<String>,
+    val servingsProduced: Number?,
+    val calories: Number?,
+    val macronutrients: Map<Macronutrient, Number>?,
+    val rating: Number?,
 )
 
-class AddAttributeToRecipeRequest (
-    val id: String,
-    val attribute: String,
-)
-
-class RemoveAttributeFromRecipeRequest (
-    val id: String,
-    val attribute: String,
-)
-
-class DeleteRecipeRequest (
-    val id: String,
-)
-
-class GetRandomizedRecipeRequest(
-    val day : String,
-    val meal : String,
-    val attributes : List<String>,
-)
-
-class GetRandomizedRecipesRequest (
+class GetRandomizedRecipeBatchRequest (
     val requests: List<GetRandomizedRecipeRequest>
 )
 
-class GetRandomizedRecipesResponse (
-    val recipes: List<Recipe>,
+/**
+ * @author Chris West
+ * As part of the MVP, this will NOT be sent on its own from the client to the backend, but will instead be packed
+ * into a list in GetRandomizedRecipeBatchRequest objects. Should we later decide that we want to allow the user to
+ * request recipes individually, this can be used for that.
+ */
+class GetRandomizedRecipeRequest(
+    /** the name of the meal will likely follow a format like "Monday-Dinner", "Tuesday-Breakfast", "Wednesday-Lunch, etc.
+     * but this allows us to have the future flexibility of allow users to name their meals whatever they want and
+     * then the backend doesn't have to care what they're called */
+    val mealName: String,
+    val tags : List<String>,
 )
 
-class GetRandomizedRecipeResponse (
-    val recipe: Recipe,
-)
+class GetRandomizedRecipeBatchResponse (
+    val recipes: List<Recipe>,
+    override val success: Boolean,
+    override val message: String
+) : Response(success)
