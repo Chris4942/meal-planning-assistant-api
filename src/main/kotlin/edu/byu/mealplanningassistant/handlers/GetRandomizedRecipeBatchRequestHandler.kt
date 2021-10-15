221ef4cfada8a0f039adffd3ddf3e1b6446f522d
sben502 <sarahxbenson@gmail.com>
2021-10-14 19:39:52 -0600

    two handler classes stubbed

---
+++ src/main/kotlin/edu/byu/mealplanningassistant/handlers/GetRandomizedRecipeBatchRequestHandler.kt
@@ -0,0 +1,13 @@
+package edu.byu.mealplanningassistant.handlers
+
+import com.sun.net.httpserver.HttpExchange
+import com.sun.net.httpserver.HttpHandler
+
+//This class should do a get request to retrieve a randomized recipe batch request and return a success
+class GetRandomizedRecipeBatchRequestHandler: HttpHandler {
+
+    override fun handle(exchange: HttpExchange?) {
+        TODO("Not yet implemented")
+    }
+
+}
