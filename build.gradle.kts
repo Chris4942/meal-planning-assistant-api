12b831cbe83a3f10a7daff26638ea8f50b56e7aa
antTolman <anttolman1@gmail.com>
2021-10-18 18:32:05 -0600

    very basic kotlin mongodb add and retrive

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.30"
+    java
+    id("org.springframework.boot") version "2.5.5"
 }
+apply(plugin = "io.spring.dependency-management")
 
 group = "me.chriswest"
 version = "1.0-SNAPSHOT"
@@ -13,6 +16,10 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.mongodb:mongo-java-driver:3.12.10")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.data:spring-data-mongodb:3.2.5")
 }
 
 tasks.test {
@@ -22,3 +29,4 @@ tasks.test {
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
+
